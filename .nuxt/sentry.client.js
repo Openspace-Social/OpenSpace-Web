912d8c8725bc949114047734f060c5f381b80652
massimoraimondi <massimo.raimondi@gmail.com>
2024-02-28 08:42:04 -0500

    added dist folder for netlify deploy

--- .nuxt/sentry.client.js
+++ .nuxt/sentry.client.js
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/browser'
 import { Dedupe, ExtraErrorData, ReportingObserver, RewriteFrames, Vue } from '@sentry/integrations'
 
 export default function (ctx, inject) {
-  const opts = Object.assign({}, {"dsn":"","environment":"development"}, {
+  const opts = Object.assign({}, {"dsn":"https:\u002F\u002Fc39e7ae67bf90aba5eade713519787a4@o4505669431197696.ingest.sentry.io\u002F4505669435850752","environment":"production"}, {
     integrations: [
       new Dedupe({}),
       new ExtraErrorData({}),
@@ -12,6 +12,9 @@ export default function (ctx, inject) {
       new Vue({Vue: VueLib, ...{"attachProps":true}})
     ]
   })
+
+  // Initialize sentry
+  Sentry.init(opts)
 
   // Inject Sentry to the context as $sentry
   inject('sentry', Sentry)
---
+++ dist/_nuxt/lang-es.dd369f0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{527:function(n,w,e){"use strict";e.r(w),w.default={name:"Okuna"}}}]);
